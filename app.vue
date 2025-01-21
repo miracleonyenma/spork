f4151f957aec2b60154f0c215c65f7ab2efd4a3a
miracleonyenma <miracleficient@gmail.com>
2025-01-21 01:24:54 +0100

    feat: update app layout and add Tailwind CSS styles

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <section class="site-section">
+      <div class="wrapper">
+        <h1>Sporks</h1>
+      </div>
+    </section>
   </div>
 </template>
