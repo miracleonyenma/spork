408cb17e943e6d326a6e519f3bb4e329cfc8b7b9
miracleonyenma <miracleficient@gmail.com>
2025-01-21 01:30:09 +0100

    feat: enhance heading style in app layout

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <NuxtRouteAnnouncer />
     <section class="site-section">
       <div class="wrapper">
-        <h1>Sporks</h1>
+        <h1 class="text-2xl">Sporks</h1>
       </div>
     </section>
   </div>
